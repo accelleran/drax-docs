ceeb6f8f25ca4f61673c453c0c4738aa7ad901f3
Esawi <91888949+Esawi@users.noreply.github.com>
2024-02-14 09:05:38 +0000

    updated core installation script

--- docs/core-install/open5gs_install_script_from_apt.sh
+++ docs/core-install/open5gs_install_script_from_apt.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+
+# MongoDB
+
 sudo apt update
 sudo apt install gnupg
 curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
-
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
@@ -10,31 +14,36 @@ sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
-Yes | sudo add-apt-repository ppa:open5gs/latest
-sudo apt update
-sudo apt install open5gs
 
+# Open5GS
+
+sudo add-apt-repository -y ppa:open5gs/latest
+sudo apt update
+sudo apt install -y open5gs
+
+
+# WebUI
 
 sudo apt install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
- # Run Update and Install
 sudo apt update
 sudo apt install -y nodejs
 
-
 curl -fsSL https://open5gs.org/open5gs/assets/webui/install | sudo -E bash -
+
+
+# WAN connectivity
 
 sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
 sudo ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE
 sudo iptables -I INPUT -i ogstun -j ACCEPT
 
-sudo apt-get install iptables-persistent
+sudo apt install -y iptables-persistent
 sudo netfilter-persistent save
 sudo systemctl enable netfilter-persistent
 
